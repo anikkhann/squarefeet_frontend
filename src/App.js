c1b52c6258551d02b1ae09843dfc459687c6449b
Anik Khan <anikkhann46@gmail.com>
2022-10-29 14:44:48 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
-import Home from './Components/Pages/Home';
-import TermsConditions from './Components/TermsConditions/TermsConditions';
-import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
-import PricingDetails from './Components/PricingDetails/PricingDetails';
-import ContactUs from './Components/ContactUs/ContactUs';
-import Hover from './Components/Hover';
+import Home from "./Components/Pages/Home";
+import TermsConditions from "./Components/TermsConditions/TermsConditions";
+import ScrollToTop from "./Components/ScrollToTop/ScrollToTop";
+import PricingDetails from "./Components/PricingDetails/PricingDetails";
+import ContactUs from "./Components/ContactUs/ContactUs";
+
 function App() {
   return (
-    <ScrollToTop >
-     <Routes>
-        {/* <Route path="/" element={<Home />} exact/> */}
-        <Route path="/" element={<Hover />} exact/>
-        <Route path="/privacy-policy" element={<TermsConditions />} exact/>
-        <Route path="/price" element={<PricingDetails />} exact/>
-        <Route path="/contact" element={<ContactUs />} exact/>
+    <ScrollToTop>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
 
+        <Route path="/privacy-policy" element={<TermsConditions />} exact />
+        <Route path="/price" element={<PricingDetails />} exact />
+        <Route path="/contact" element={<ContactUs />} exact />
       </Routes>
     </ScrollToTop>
   );
